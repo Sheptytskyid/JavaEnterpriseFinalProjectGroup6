de10aba02a7b258588e1c38b9c0d50eff99dd754
Denys Sheptytskyi <neoxesis@mail.ru>
2017-04-25 20:29:57 +0300

    added Spring Boot launcher

--- src/main/java/net/greatstart/controllers/MainController.java
+++ src/main/java/net/greatstart/controllers/MainController.java
@@ -2,11 +2,15 @@ package net.greatstart.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
+
     @RequestMapping(value = "/")
-    public String frontPage() {
-        return "index";
+    public ModelAndView frontPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        return modelAndView;
     }
 }
