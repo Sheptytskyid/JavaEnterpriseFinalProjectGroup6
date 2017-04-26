b1df383ff05f8804d8bc9184f852298a44f5cacb
Denis Azhymov <azhimov93@gmail.com>
2017-04-26 21:29:44 +0300

    [#38] Hibernate with comments

--- src/main/java/net/greatstart/configs/DataBaseConfig.java
+++ src/main/java/net/greatstart/configs/DataBaseConfig.java
@@ -4,19 +4,17 @@ import org.apache.commons.dbcp.BasicDataSource;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
 @Configuration
-@ComponentScan("net.greatstart.*")
 @EnableTransactionManagement
 @PropertySource(value = {"classpath:database.properties"})
 public class DataBaseConfig {
