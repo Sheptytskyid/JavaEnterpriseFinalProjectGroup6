4d2e4388467bb9945304878eac5922bb57484d29
Denis Azhymov <azhimov93@gmail.com>
2017-04-27 14:38:52 +0300

    [#38] Hibernate with comments add Transaction

--- src/main/java/net/greatstart/services/InvestmentService.java
+++ src/main/java/net/greatstart/services/InvestmentService.java
@@ -4,10 +4,12 @@ import net.greatstart.dao.InvestmentDao;
 import net.greatstart.model.Investment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 public class InvestmentService {
 
     private InvestmentDao investmentDao;
