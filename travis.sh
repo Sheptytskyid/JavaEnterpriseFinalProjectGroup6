b7660bc4f76abd9d8368431deeaf662686d9af46
Denys Sheptytskyi <neoxesis@mail.ru>
2017-04-28 22:21:35 +0300

    avoid script execution in master branch

--- travis.sh
+++ travis.sh
@@ -8,7 +8,7 @@ fi
 
 ZOZICH_REVIEWS=$(curl -X GET -H "Accept: application/vnd.github.black-cat-preview+json" https://api.github.com/repos/Sheptytskyid/Startup_Platform/pulls/$TRAVIS_PULL_REQUEST/reviews | jq '.[] | select(.user.login == "zozich") | .state')
 
-if [[ "$ZOZICH_REVIEWS" == *"APPROVED" ]]
+if [[ "$ZOZICH_REVIEWS" == *\"APPROVED\" ]]
 then state="success" desc="Zozich has approved this PR!"
 else state="failure" desc="Zozich has not approved this PR!"
 fi
