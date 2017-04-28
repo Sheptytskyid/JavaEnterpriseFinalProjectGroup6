851de0596fd7be3e10931bf084577301125e271d
Denys Sheptytskyi <neoxesis@mail.ru>
2017-04-28 18:55:31 +0300

    updated script

--- travis.sh
+++ travis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(curl -X GET -H "Accept: application/vnd.github.black-cat-preview+json" https://api.github.com/repos/Sheptytskyid/Startup_Platform/pulls/$TRAVIS_PULL_REQUEST/reviews | jq '.[] | select(.user.login == "zozich") | .state == "APPROVED"') ];
+if [ $(curl -X GET -H "Accept: application/vnd.github.black-cat-preview+json" https://api.github.com/repos/Sheptytskyid/Startup_Platform/pulls/$TRAVIS_PULL_REQUEST/reviews | jq '.[] | select(.user.login == "zozich") | .state == "APPROVED"') == true ];
 then state="success" desc="Zozich has approved this PR!";
 else state="failure" desc="Zozich has not approved this PR!";
 fi
