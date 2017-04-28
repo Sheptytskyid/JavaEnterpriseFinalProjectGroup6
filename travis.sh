c0521778e16737e8147ef11170cbe587bdd3b286
Denys Sheptytskyi <neoxesis@mail.ru>
2017-04-28 18:38:30 +0300

    test

---
+++ travis.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $(curl -X GET -H "Accept: application/vnd.github.black-cat-preview+json" https://api.github.com/repos/Sheptytskyid/Startup_Platform/pulls/$TRAVIS_PULL_REQUEST/reviews | jq '.[] | select(.user.login == "zozich") | .state == "APPROVED"') == true ];
+then state="success" desc="Zozich has approved this PR!";
+else state="failure" desc="Zozich has not approved this PR!";
+fi
+curl -H "Authorization: token $GITHUB_TOKEN" -d "{
+  \"state\": \"$state\",
+  \"description\": \"$desc\",
+  \"context\": \"Zozich appoval\"
+}" https://api.github.com/repos/Sheptytskyid/Startup_Platform/statuses/$TRAVIS_PULL_REQUEST_SHA
