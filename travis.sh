13ea64bf6127f2931c3a9ce7b7bea049a574a443
Denys Sheptytskyi <neoxesis@mail.ru>
2017-04-28 22:15:20 +0300

    avoid script execution in master branch

--- travis.sh
+++ travis.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 echo $TRAVIS_PULL_REQUEST_BRANCH
+
 if [ $TRAVIS_PULL_REQUEST_BRANCH == "master" ]
 then exit
 fi
-if [ $(curl -X GET -H "Accept: application/vnd.github.black-cat-preview+json" https://api.github.com/repos/Sheptytskyid/Startup_Platform/pulls/$TRAVIS_PULL_REQUEST/reviews | jq '.[] | select(.user.login == "zozich") | .state == "APPROVED"') == true ];
-then state="success" desc="Zozich has approved this PR!";
-else state="failure" desc="Zozich has not approved this PR!";
+
+ZOZICH_REVIEWS=$(curl -X GET -H "Accept: application/vnd.github.black-cat-preview+json" https://api.github.com/repos/Sheptytskyid/Startup_Platform/pulls/21/reviews | jq '.[] | select(.user.login == "zozich") | .state')
+
+if [[ "$ZOZICH_REVIEWS" == *"APPROVED" ]]
+then state="success" desc="Zozich has approved this PR!"
+else state="failure" desc="Zozich has not approved this PR!"
 fi
+
 curl -H "Authorization: token $GITHUB_TOKEN" -d "{
   \"state\": \"$state\",
   \"description\": \"$desc\",
