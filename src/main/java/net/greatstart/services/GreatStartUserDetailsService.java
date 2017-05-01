5004c63ec50b472c76a0f5a716c0ff156e83c9db
holkwen <vlad.kryshtalov@gmx.com>
2017-05-01 09:57:22 +0300

    Added autowired

--- src/main/java/net/greatstart/services/GreatStartUserDetailsService.java
+++ src/main/java/net/greatstart/services/GreatStartUserDetailsService.java
@@ -1,6 +1,7 @@
 package net.greatstart.services;
 
 import net.greatstart.model.Role;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
@@ -17,6 +18,7 @@ public class GreatStartUserDetailsService implements UserDetailsService {
 
     private UserService userService;
 
+    @Autowired
     public GreatStartUserDetailsService(UserService userService) {
         this.userService = userService;
     }
