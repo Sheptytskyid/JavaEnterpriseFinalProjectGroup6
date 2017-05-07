668ba48356b8a3c1cb915f05aee4d718a7f21738
semyonov-m-y <semyonov-m-y@mail.ru>
2017-05-07 20:28:04 +0300

    [#42] Develop site menu - v03

--- src/main/webapp/resources/js/colored-div.js
+++
@@ -1,9 +0,0 @@
-$(window).scroll(function() {
-if ($(this).scrollTop() > 1){  
-    $('.hamburger.is-closed').addClass("sticky");
-  }
-  else{
-    $('.hamburger.is-closed').removeClass("sticky");
-  }
-});
-	
