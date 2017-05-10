7a74cedf335477b1b78761b62e6cf900f562d8de
Denys Sheptytskyi <neoxesis@mail.ru>
2017-05-10 16:57:09 +0300

    fixed badges in readme.md added PowerMock dependency in pom.xml added processRegistration test to RegistrationControllerTest

--- src/test/java/net/greatstart/controllers/RegistrationControllerTest.java
+++ src/test/java/net/greatstart/controllers/RegistrationControllerTest.java
@@ -1,28 +1,46 @@
 package net.greatstart.controllers;
 
+import net.greatstart.dto.DtoUser;
 import net.greatstart.services.SecurityService;
 import net.greatstart.services.UserService;
+import net.greatstart.validators.UserValidationService;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.validation.Errors;
+import org.springframework.web.servlet.ModelAndView;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(UserValidationService.class)
 public class RegistrationControllerTest {
+
+    private static final String EMAIL = "a@example.com";
+    private static final String PASS = "pass";
+
     @Mock
     private UserService userService;
     @Mock
     private PasswordEncoder passwordEncoder;
     @Mock
     private SecurityService securityService;
+    @Mock
+    private Errors errors;
+
     @InjectMocks
     private RegistrationController controller;
     private MockMvc mvc;
@@ -36,4 +54,19 @@ public class RegistrationControllerTest {
     public void registerShouldReturnView() throws Exception {
         mvc.perform(get("/user/register")).andExpect(view().name("login/registration"));
     }
+
+    @Test(timeout = 2000)
+    public void processRegistration() {
+        DtoUser user = new DtoUser();
+        user.setEmail(EMAIL);
+        user.setPassword(PASS);
+        mockStatic(UserValidationService.class);
+        ModelAndView modelAndView = controller.processRegistration(user, errors);
+        verifyStatic(times(1));
+        UserValidationService.validate(user, errors, userService);
+        verify(userService, times(1)).createUser(user.getEmail(), passwordEncoder.encode(user.getPassword()));
+        verify(securityService, times(1)).autoLogin(user.getEmail(), user.getPassword());
+        assertEquals(modelAndView.getViewName(), "redirect:/");
+    }
+
 }
