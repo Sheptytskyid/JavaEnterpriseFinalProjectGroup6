840780bba00e3ef3284ab3fe45128cbb5aaafb23
Denis Azhymov <azhimov93@gmail.com>
2017-05-11 16:29:16 +0300

    angular first samples

--- src/main/webapp/resources/app/controllers/controllers.js
+++ src/main/webapp/resources/app/controllers/controllers.js
@@ -1,7 +1,31 @@
-app.controller('MainController', function ($scope) {
-    init();
-    function init() {
 
-    }
-
+var demoApp = angular.module('greatStartApp', ['ngRoute']);
+demoApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/',
+            {
+                controller: 'SimpleController',
+                templateUrl: 'views/Main.html'
+            })
+        .when('/userPage', {
+            controller: 'SimpleController',
+            templateUrl: 'views/UserPage.html'
+        })
+        .otherwise({redirectTo: '/'});
 });
+
+demoApp.controller('SimpleController', ['$scope', function ($scope) {
+    $scope.customers = [
+        {name: 'Dave Jones', city: 'Phoenix'},
+        {name: 'Jamie Riley', city: 'Atlanta'},
+        {name: 'Heedy Wahlin', city: 'Chandler'},
+        {name: 'Thomas Winter', city: 'Seattle'}
+    ];
+    $scope.addCustomer = function () {
+        $scope.customers.push(
+            {
+                name: $scope.newCustomer.name,
+                city: $scope.newCustomer.city
+            });
+    };
+}]);
