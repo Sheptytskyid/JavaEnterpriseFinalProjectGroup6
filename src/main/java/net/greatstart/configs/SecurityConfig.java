bb453b0b77d6d9e07fe7934b53495e0ec5e83e38
Denis Azhymov <azhimov93@gmail.com>
2017-05-12 16:33:41 +0300

    angular first samples

--- src/main/java/net/greatstart/configs/SecurityConfig.java
+++ src/main/java/net/greatstart/configs/SecurityConfig.java
@@ -37,10 +37,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/user/login")
                 .usernameParameter("email")
                 // TODO: change the routing when home page is ready
-                //TODO: uncomment this line: .defaultSuccessUrl("/project/")
+                .defaultSuccessUrl("/project/")
                 .and()
                 .logout()
-                //TODO: uncomment this line: .logoutSuccessUrl("/project/")
+                .logoutSuccessUrl("/project/")
                 .and()
                 .rememberMe()
                 .key("greatStartKey")
