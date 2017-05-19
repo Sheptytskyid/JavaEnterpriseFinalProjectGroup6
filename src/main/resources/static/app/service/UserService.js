e746dd98e89fce2d4bb4b3550a49c36b693ae23f
Denis Azhymov <azhimov93@gmail.com>
2017-05-19 16:15:23 +0300

    testlogin

---
+++ src/main/resources/static/app/service/UserService.js
@@ -0,0 +1,21 @@
+var userService = angular.module('greatStartApp').factory('userService', function ($rootScope, $http) {
+    return {
+        authenticate: function (credentials, callback) {
+
+            var headers = credentials ? {
+                authorization: "Basic " + btoa(credentials.email + ":" + credentials.password)
+            } : {};
+            $http.get('user', {headers: headers}).then(function (response) {
+                if (response.data.name) {
+                    $rootScope.authenticated = true;
+                } else {
+                    $rootScope.authenticated = false;
+                }
+                callback && callback();
+            }, function () {
+                $rootScope.authenticated = false;
+                callback && callback();
+            });
+        }
+    }
+});
--- src/main/resources/static/views/main/MainController.js
+++ src/main/resources/static/views/main/MainController.js
@@ -1,6 +1,6 @@
 var MainController = angular.module('greatStartApp')
     .controller('MainController',
-        ['$location', '$scope', '$uibModal', '$http', '$rootScope', function ($location, $scope, $uibModal, $http, $rootScope) {
+        ['$location', 'userService', '$scope', '$uibModal', '$http', '$rootScope', function ($location, userService, $scope, $uibModal, $http, $rootScope) {
             var modalPopup = function () {
                 return $scope.modalInstance = $uibModal.open({
                     templateUrl: 'views/main/LoginPage.html',
@@ -26,44 +26,6 @@ var MainController = angular.module('greatStartApp')
                     $rootScope.authenticated = false;
                 });
             };
-        }])
-    .controller('LoginController',
-        function ($rootScope, $scope, $http, $location) {
-            $scope.close = function () {
-                $scope.modalInstance.close();
-            };
 
-            var authenticate = function (credentials, callback) {
-
-                var headers = credentials ? {
-                    authorization: "Basic " + btoa(credentials.email + ":" + credentials.password)
-                } : {};
-                $http.get('user', {headers: headers}).then(function (response) {
-                    if (response.data.name) {
-                        $rootScope.authenticated = true;
-                    } else {
-                        $rootScope.authenticated = false;
-                    }
-                    callback && callback();
-                }, function () {
-                    $rootScope.authenticated = false;
-                    callback && callback();
-                });
-
-            };
-            authenticate();
-
-            $scope.credentials = {};
-            $scope.login = function () {
-                authenticate($scope.credentials, function () {
-                    if ($rootScope.authenticated) {
-                        $location.path("/");
-                        $scope.error = false;
-                        $scope.close();
-                    } else {
-                        $scope.error = true;
-                    }
-                })
-            };
-
-        });
+            userService.authenticate();
+        }]);
