97256e82ce93eef5fe9e66482b7e674e5fda68b0
Denys Azhymov <azhimov93@gmail.com>
2017-05-20 21:21:48 +0300

    [#73] Implement Login/Logout Spring Security + AngularJs (#52)
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin
    
    * testlogin

--- src/main/resources/static/views/main/MainController.js
+++ src/main/resources/static/views/main/MainController.js
@@ -1,24 +1,30 @@
 var MainController = angular.module('greatStartApp')
-    .controller('MainController', ['$scope', '$uibModal', function ($scope, $uibModal) {
-        var modalPopup = function () {
-            return $scope.modalInstance = $uibModal.open({
-                templateUrl: 'views/main/LoginPage.html',
-                size: 'sm',
-                backdrop: true,
-                scope: $scope
-            });
-        };
-
-        $scope.openPopup = function () {
-            modalPopup().result
-                .then(function () {
-                })
-                .then(null, function () {
+    .controller('MainController',
+        ['$location', 'userService', '$scope', '$uibModal', '$http', '$rootScope', function ($location, userService, $scope, $uibModal, $http, $rootScope) {
+            var modalPopup = function () {
+                return $scope.modalInstance = $uibModal.open({
+                    templateUrl: 'views/main/LoginPage.html',
+                    controller: 'LoginController',
+                    size: 'sm',
+                    backdrop: 'static',
+                    scope: $scope
                 });
-        };
+            };
 
-        $scope.close = function () {
-            $scope.modalInstance.close();
-        };
+            $scope.openPopup = function () {
+                modalPopup().result
+                    .then(function () {
+                    })
+                    .then(null, function () {
+                    });
+            };
 
-    }]);
+            $scope.logout = function () {
+                $http.post('logout', {}).finally(function () {
+                    $location.path("/");
+                    $rootScope.authenticated = false;
+                });
+            };
+
+            userService.authenticate();
+        }]);
