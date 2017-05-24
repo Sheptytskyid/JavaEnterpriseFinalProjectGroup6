809a012fb8d910070a35447c60f3c752dda6e320
Denis Azhymov <azhimov93@gmail.com>
2017-05-24 16:32:38 +0300

    restore

--- src/main/java/net/greatstart/controllers/UserRestController.java
+++ src/main/java/net/greatstart/controllers/UserRestController.java
@@ -5,23 +5,23 @@ import net.greatstart.dto.DtoUserProfile;
 import net.greatstart.model.User;
 import net.greatstart.services.UserConverterService;
 import net.greatstart.services.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
-import java.io.IOException;
 import java.security.Principal;
 
 @RestController
 public class UserRestController {
     private UserService userService;
     private UserConverterService userConverter;
-    private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     public UserRestController(UserService userService, UserConverterService userConverter) {
@@ -54,37 +54,8 @@ public class UserRestController {
         return new ResponseEntity<DtoUserProfile>(HttpStatus.NOT_FOUND);
     }
 
-//    @PutMapping("/api/user/{id}")
-//    public ResponseEntity<DtoUserProfile> updateUser(@PathVariable("id") long id, @Valid @RequestBody DtoUserProfile dtoUser) {
-//        DtoUserProfile currentDtoUser = new DtoUserProfile();
-//        User currentUser = userService.getUserById(id);
-//        if (currentUser != null && id == currentUser.getId()) {
-//            User entityUser = userService.getUserById(id);
-//            userConverter.updateUserFromDto(entityUser, dtoUser);
-//            userService.updateUser(entityUser);
-//            currentDtoUser = userConverter.fromUserToDtoProfile(entityUser);
-//        }
-//        return new ResponseEntity<DtoUserProfile>(currentDtoUser, HttpStatus.OK);
-//    }
-
     @PutMapping("/api/user/{id}")
-    public ResponseEntity<DtoUserProfile> updateUser(@PathVariable("id") long id,
-                                                     @Valid @RequestBody DtoUserProfile dtoUser,
-                                                     @RequestParam(value = "file", required = false) MultipartFile file) {
-
-        if (file != null && !file.isEmpty()) {
-            byte[] content = null;
-            try {
-                logger.info("File name: " + file.getName());
-                logger.info("File size: " + file.getSize());
-                logger.info("File content type: " + file.getContentType());
-                content = file.getBytes();
-                dtoUser.setPhoto(content);
-            } catch (IOException e) {
-                logger.error("Error saving upload file", e);
-            }
-            dtoUser.setPhoto(content);
-        }
+    public ResponseEntity<DtoUserProfile> updateUser(@PathVariable("id") long id, @Valid @RequestBody DtoUserProfile dtoUser) {
         DtoUserProfile currentDtoUser = new DtoUserProfile();
         User currentUser = userService.getUserById(id);
         if (currentUser != null && id == currentUser.getId()) {
@@ -95,4 +66,5 @@ public class UserRestController {
         }
         return new ResponseEntity<DtoUserProfile>(currentDtoUser, HttpStatus.OK);
     }
+
 }
--- src/main/resources/static/views/user/UserController.js
+++ src/main/resources/static/views/user/UserController.js
@@ -5,13 +5,10 @@ var UserController = angular.module('greatStartApp')
         $scope.myImage = '';
         $scope.myCroppedImage = '';
 
-        $scope.$on('LOAD', function () {
-            $scope.loading = true;
+        $scope.user = angular.copy($rootScope.currentUser);
+        $rootScope.$watch('currentUser', function () {
+            $scope.user = angular.copy($rootScope.currentUser);
         });
-        $scope.$on('UNLOAD', function () {
-            $scope.loading = false;
-        });
-
         var handleFileSelect = function (evt) {
             var file = evt.currentTarget.files[0];
             var reader = new FileReader();
@@ -20,33 +17,19 @@ var UserController = angular.module('greatStartApp')
                     $scope.myImage = evt.target.result;
                 });
             };
+
             reader.readAsDataURL(file);
+
         };
+
         angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
 
-        function dataURItoBlob(dataURI) {
-            var byteString = atob(dataURI.split(',')[1]);
-            var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-            var ab = new ArrayBuffer(byteString.length);
-            var ia = new Uint8Array(ab);
-            for (var i = 0; i < byteString.length; i++) {
-                ia[i] = byteString.charCodeAt(i);
-            }
-
-            var blob = new Blob([ab], {type: mimeString});
-            return blob;
-        }
-
-
-        $scope.user = angular.copy($rootScope.currentUser);
-        $rootScope.$watch('currentUser', function () {
-            $scope.user = angular.copy($rootScope.currentUser);
-            console.log("Copy from $rootScope", $scope.user);
-        });
-
         $scope.update = function (user) {
-            user.photo = dataURItoBlob($scope.myCroppedImage);
-            User.update({id: user.id}, user, function () {
+            if ($scope.myCroppedImage !== null) {
+                console.log(myCroppedImage);
+                user.photo = $scope.myCroppedImage.replace(/^data:image\/[a-z]+;base64,/, "");
+            }
+            User.update({id: user.id}, user, $scope.myCroppedImage, function () {
                 User.get({id: user.id});
             })
         };
@@ -56,14 +39,9 @@ var UserController = angular.module('greatStartApp')
                 console.log('Saving New User', $scope.user);
                 $scope.createUser();
             } else {
-
-                console.log('Upddating user with id ', $scope.user.id);
                 $scope.update($scope.user);
-                console.log('User updated with id ', $scope.user.id);
-                $scope.$emit('LOAD');
-                // $window.location.reload();
-                // $location.path('/user/:id', $scope.user.id);
-                $scope.$emit('UNLOAD');
+                $location.path('/user/:id', $scope.user.id);
+                $window.location.reload();
             }
         };
 
@@ -71,7 +49,7 @@ var UserController = angular.module('greatStartApp')
             User.get({id: $scope.user.id});
         }
 
-        //TODO write create method for todo
+        //TODO write create method for user
 
 
     });
