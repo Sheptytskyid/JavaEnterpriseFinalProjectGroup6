dfa14f1564b9f7babfd289099c9d72f987c5a6f9
Denys Azhymov <azhimov93@gmail.com>
2017-05-25 15:50:31 +0300

    Completly migrate UserPage and EditUserPage to angularjs (#57)
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * init
    
    * restore
    
    * restore
    
    * restore
    
    * restore
    
    * restore
    
    * restore
    
    * restore
    
    * Merge branch 'master' into userPage
    
    * Merge branch 'master' into userPage
    
    * init
    
    * init
    
    * finish!
    
    * finish!
    
    * finish!
    
    * finish!
    
    * finish!
    
    * finish!
    
    * finish!
    
    * finish!
    
    * finish!

--- src/main/resources/static/views/user/UserController.js
+++ src/main/resources/static/views/user/UserController.js
@@ -1,8 +1,18 @@
 var UserController = angular.module('greatStartApp')
-    .controller('UserController', ['$scope', function ($scope) {
+    .controller('UserController', function ($scope, $rootScope, User, $location) {
+
         $scope.flag = false;
         $scope.myImage = '';
         $scope.myCroppedImage = '';
+        $scope.isImageChange = false;
+
+        //TODO: this method retrieve currentUser. Need to think how to rewrite it in right way
+        $scope.user = angular.copy($rootScope.currentUser);
+        $rootScope.$watch('currentUser', function () {
+            $scope.user = angular.copy($rootScope.currentUser);
+        });
+
+
 
         var handleFileSelect = function (evt) {
             var file = evt.currentTarget.files[0];
@@ -10,9 +20,46 @@ var UserController = angular.module('greatStartApp')
             reader.onload = function (evt) {
                 $scope.$apply(function ($scope) {
                     $scope.myImage = evt.target.result;
+                    $scope.isImageChange = true;
                 });
             };
+
             reader.readAsDataURL(file);
+
         };
+
         angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
-    }]);
+
+        $scope.update = function (user) {
+            return $scope.user = User.update({id: user.id}, user, function () {
+                $rootScope.currentUser = User.get({id: user.id})
+            })
+        };
+
+        $scope.submit = function () {
+            if ($scope.user.id === null) {
+                console.log('Saving New User', $scope.user);
+                $scope.createUser();
+            } else {
+                if ($scope.isImageChange) {
+                    $scope.user.photo = $scope.myCroppedImage.replace(/^data:image\/[a-z]+;base64,/, "");
+                }
+                $scope.update($scope.user).$promise.then(function (success) {
+                    $location.path('/user/:id', $scope.user.id);
+                },function (error) {
+                    $scope.error=true;
+                });
+
+            }
+        };
+
+        //TODO write create method for user
+        $scope.createUser=function () {
+
+        };
+
+        $scope.getUser = function () {
+            User.get({id: $scope.user.id});
+        }
+
+    });
--- src/test/java/net/greatstart/controllers/UserControllerTest.java
+++ src/test/java/net/greatstart/controllers/UserControllerTest.java
@@ -1,5 +1,7 @@
 package net.greatstart.controllers;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import net.greatstart.dto.DtoUserProfile;
 import net.greatstart.mappers.UserProfileMapper;
 import net.greatstart.model.User;
@@ -10,129 +12,110 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
+
+import static net.greatstart.MapperHelper.getTestDtoUserProfile;
+import static net.greatstart.MapperHelper.getTestUser;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 import java.security.Principal;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
 
-    private static final String SHOW_PROFILE = "user/UserPage";
-    private static final String EDIT_PROFILE = "user/EditUserPage";
-    private static final String ERROR_PROFILE = "redirect:/";
     private static final long ID = 1L;
-    private static final String TEST_USER_PROFILE = "/user/" + ID;
-    private static final String TEST_EMAIL = "";
+    private static final String TEST_USER_PROFILE = "/api/user/" + ID;
 
+
+    @Mock
+    private Principal principal;
     @Mock
     private UserService userService;
     @Mock
     private UserProfileMapper userMapper;
-    @Mock
-    private Principal principal;
-    @Mock
-    private BindingResult bindingResult;
-    @Mock
-    private MultipartFile file;
+
     @InjectMocks
     private UserController controller;
     private MockMvc mvc;
-    private User user;
+    private User user = getTestUser();
+    private DtoUserProfile dtoUser = getTestDtoUserProfile();
 
-    @Before
-    public void setUp() {
-        user = new User();
-    }
 
     @Before
     public void init() {
         mvc = standaloneSetup(controller).build();
     }
 
-    @Test(timeout = 2000)
-    public void showProfileAndCheckView() throws Exception {
+    @Test
+    public void findById_UserNotFound_ShouldReturnHttpStatusCode404() throws Exception {
+        when(userService.getUserById(ID)).thenReturn(null);
+        mvc.perform(get(TEST_USER_PROFILE)).andExpect(status().isNotFound());
+        verify(userService, times(1)).getUserById(ID);
+        verifyNoMoreInteractions(userService);
+
+    }
+
+    @Test
+    public void findById_UserFound_ShouldReturnFoundUserEntity() throws Exception {
+        when(userService.getUserById(ID)).thenReturn(dtoUser);
         mvc.perform(get(TEST_USER_PROFILE))
-                .andExpect(view().name(SHOW_PROFILE));
+                .andExpect(status().isOk());
+        verify(userService, times(1)).getUserById(ID);
+        verifyNoMoreInteractions(userService);
     }
 
-    @Test(timeout = 2000)
-    public void showProfileAndVerifyInteractions() throws Exception {
-        user.setId(ID);
-        when(userService.getUserById(ID)).thenReturn(user);
-        when(userMapper.fromUserToDtoProfile(user)).thenReturn(new DtoUserProfile());
-        mvc.perform(get(TEST_USER_PROFILE))
-                .andExpect(view().name(SHOW_PROFILE));
-        verify(userService).getUserById(ID);
-        verify(userMapper).fromUserToDtoProfile(user);
+    @Test
+    public void updateUser_UserSuccessfullyUpdatedAndSuccessfullyPassValidation_ShouldReturnUpdatedUser()
+            throws Exception {
+        dtoUser.setName("TestName");
+        when(userService.updateUser(dtoUser, ID)).thenReturn(dtoUser);
+        mvc.perform(put(TEST_USER_PROFILE).contentType(MediaType.APPLICATION_JSON).content(convertObjectToJsonBytes(dtoUser)))
+                .andExpect(status().isOk());
+        verify(userService, times(1)).updateUser(dtoUser, ID);
+        verifyNoMoreInteractions(userService);
     }
 
-    @Test(timeout = 2000)
-    public void getEditProfileFormByWrongUserExpectErrorPage() throws Exception {
-        mvc.perform(get(TEST_USER_PROFILE + "/edit"))
-                .andExpect(view().name(ERROR_PROFILE));
+    @Test
+    public void updateUser_withWrongId_ShouldReturnCode404()
+            throws Exception {
+        dtoUser.setName("TestName");
+        dtoUser.setId(2L);
+        when(userService.updateUser(dtoUser, ID)).thenReturn(null);
+        mvc.perform(put(TEST_USER_PROFILE).contentType(MediaType.APPLICATION_JSON).content(convertObjectToJsonBytes(dtoUser)))
+                .andExpect(status().isNotFound());
+        verify(userService, times(1)).updateUser(dtoUser, ID);
+        verifyNoMoreInteractions(userService);
     }
 
-    @Test(timeout = 2000)
-    public void getEditProfile() throws Exception {
-        user.setEmail(TEST_EMAIL);
-        when(userService.getUserById(ID)).thenReturn(user);
-        when(userMapper.fromUserToDtoProfile(user)).thenReturn(new DtoUserProfile());
-        when(principal.getName()).thenReturn(TEST_EMAIL);
-        mvc.perform(get(TEST_USER_PROFILE + "/edit").principal(principal))
-                .andExpect(view().name(EDIT_PROFILE));
-        verify(userService).getUserById(ID);
-        verify(userMapper).fromUserToDtoProfile(user);
+    @Test
+    public void updateUser_FailValidation_ShouldReturnCode400() throws Exception {
+        dtoUser.setName("Fail");
+        when(userService.getUserById(ID)).thenReturn(dtoUser);
+        mvc.perform(put(TEST_USER_PROFILE).contentType(MediaType.APPLICATION_JSON).content(convertObjectToJsonBytes(dtoUser)))
+                .andExpect(status().isBadRequest());
+        verifyNoMoreInteractions(userService);
     }
 
-    @Test(timeout = 2000)
-    public void saveInvalidEditedProfileExpectReturningToEditPage() throws Exception {
-        mvc.perform(post(TEST_USER_PROFILE + "/edit"))
-                .andExpect(view().name(EDIT_PROFILE));
+
+    @Test
+    public void user() throws Exception {
+        assertEquals(principal, controller.user(principal));
+
     }
 
-    @Test(timeout = 2000)
-    public void saveEditedProfileWrongUser() throws Exception {
-        ModelAndView modelAndView = controller
-                .saveEditedProfile(ID, new DtoUserProfile(), bindingResult, principal, file);
-
-        assertEquals("redirect:" + TEST_USER_PROFILE, modelAndView.getViewName());
-        verify(userService).getUserByEmail(null);
-    }
-
-    @Test(timeout = 2000)
-    public void saveEditedProfileCorrectUser() throws Exception {
-        DtoUserProfile dtoUser = new DtoUserProfile();
-        User user = new User();
-        user.setId(ID);
-        when(userService.getUserByEmail(null)).thenReturn(user);
-        when(userService.getUserById(ID)).thenReturn(user);
-        when(userMapper.fromDtoProfileToUser(dtoUser)).thenReturn(user);
-        ModelAndView modelAndView = controller
-                .saveEditedProfile(ID, dtoUser, bindingResult, principal, file);
-        verify(userMapper).fromDtoProfileToUser(dtoUser);
-        verify(userService).updateUser(user);
-        assertEquals("redirect:" + TEST_USER_PROFILE, modelAndView.getViewName());
-    }
-
-    @Test(timeout = 2000)
-    public void downloadPhoto() throws Exception {
-        byte[] bytes = new byte[] {0, 1, 0};
-        DtoUserProfile dtoUserProfile = new DtoUserProfile();
-        dtoUserProfile.setPhoto(bytes);
-        when(userService.getUserById(ID)).thenReturn(user);
-        when(userMapper.fromUserToDtoProfile(user)).thenReturn(dtoUserProfile);
-        mvc.perform(get("/user/photo/1")).andExpect(content().bytes(bytes));
+    private byte[] convertObjectToJsonBytes(Object object) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper.writeValueAsBytes(object);
     }
 }
