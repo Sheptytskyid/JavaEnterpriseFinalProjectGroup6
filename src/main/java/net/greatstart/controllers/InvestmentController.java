45d4d9ec79581ab7980d72f8fa7e522bc906253e
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2017-05-25 23:53:23 +0300

    some design and checkstyle fix

--- src/main/java/net/greatstart/controllers/InvestmentController.java
+++ src/main/java/net/greatstart/controllers/InvestmentController.java
@@ -11,7 +11,12 @@ import net.greatstart.validators.InvestmentValidationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.math.BigDecimal;
