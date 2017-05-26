0cb069958f858ae27036588d1dedefada891395e
semyonov-m-y <semyonov-m-y@mail.ru>
2017-05-26 15:36:00 +0300

    [#80] Design admin page - v01

--- src/main/resources/static/admin/adminjs/jquery.equalHeight.js
+++ src/main/resources/static/admin/adminjs/jquery.equalHeight.js
@@ -5,9 +5,9 @@
         $.fn.equalHeight = function() {
            // find the tallest height in the collection
            // that was passed in (.column)
-            tallest = 0;
+            var tallest = 0;
             this.each(function(){
-                thisHeight = $(this).height();
+                var thisHeight = $(this).height();
                 if( thisHeight > tallest)
                     tallest = thisHeight;
             });
