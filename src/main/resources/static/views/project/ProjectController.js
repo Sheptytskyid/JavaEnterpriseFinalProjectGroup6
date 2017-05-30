2d181eb6cc40891a6d29e191c70020c16e5a345a
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2017-05-30 21:31:47 +0300

    Project page totally mapped

--- src/main/resources/static/views/project/ProjectController.js
+++ src/main/resources/static/views/project/ProjectController.js
@@ -1,32 +1,30 @@
 var ProjectController = angular.module('greatStartApp')
     .controller('ProjectController', function ($scope, $routeParams, Project) {
+        var investedAmount = function (project) {
+            var result = 0;
+            for (var i = 0; i < project.dtoInvestments.length; i++) {
+                if (project.dtoInvestments[i].paid) {
+                    result += project.dtoInvestments[i].sum;
+                }
+            }
+            return result;
+        };
+
+        var ivnProgressWithWidth = function (investedAmount, project) {
+            return 'width: ' + investedAmount * 100 / project.desc.cost + '%';
+        };
+
         $scope.getProject = function () {
             var project = Project.get({id: $routeParams.id}, function () {
-                console.log($scope.id);
                 $scope.project = project;
-            }, function (error) {
-                console.log(error)
+                $scope.investedAmount = investedAmount(project);
+                $scope.invProgressWithWidth = ivnProgressWithWidth($scope.investedAmount, project);
             });
         };
-
-        var investedAmount = function (project) {
-            console.log(project);
-            var sum = 0;
-            // for (var i = 0; i < $scope.project.dtoInvestments.length; i++) {
-            //     sum += project.dtoInvestments[i].sum;
-            // }
-            return sum;
-        };
-
-        $scope.investedAmount = investedAmount();
 
         $scope.getAllProjects = function () {
             var projects = Project.query({}, function () {
                 $scope.projects = projects;
             });
         };
-
-        // $scope.ivnProgressWithWidth = function () {
-        //     return 'width: ' + investedAmount() * 100 / project.desc.cost;
-        // };
     });
