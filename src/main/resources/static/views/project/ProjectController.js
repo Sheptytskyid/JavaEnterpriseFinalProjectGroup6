a7d0d83fdeca41f54f6b000538b90aaaf6456c79
holkwen <vlad.kryshtalov@gmx.com>
2017-05-31 22:11:49 +0300

    Edit project images

--- src/main/resources/static/views/project/ProjectController.js
+++ src/main/resources/static/views/project/ProjectController.js
@@ -1,17 +1,17 @@
 var ProjectController = angular.module('greatStartApp')
-    .controller('ProjectController', function ($scope, Project, $routeParams) {
+    .controller('ProjectController', function ($scope, $rootScope, Project, $routeParams, $location) {
 
-        $scope.myImage = '';
-        $scope.myCroppedImage = '';
-        $scope.isImageChange = false;
+        $scope.projectImage = '';
+        $scope.projectCroppedImage = '';
+        $scope.imageChanged = false;
 
-        var handleFileSelect = function (evt) {
+        var handleProjectImageSelect = function (evt) {
             var file = evt.currentTarget.files[0];
             var reader = new FileReader();
             reader.onload = function (evt) {
                 $scope.$apply(function ($scope) {
-                    $scope.myImage = evt.target.result;
-                    $scope.isImageChange = true;
+                    $scope.projectImage = evt.target.result;
+                    $scope.imageChanged = true;
                 });
             };
 
@@ -19,7 +19,24 @@ var ProjectController = angular.module('greatStartApp')
 
         };
 
-        angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
+        angular.element(document.querySelector('#fileInput')).on('change', handleProjectImageSelect);
+
+        $scope.update = function (project) {
+            return $scope.project = Project.update({id: project.id}, project, function () {
+                $scope.project = Project.get({id: project.id})
+            })
+        };
+
+        $scope.submit = function() {
+            if ($scope.imageChanged) {
+                $scope.project.desc.image = $scope.projectCroppedImage.replace(/^data:image\/[a-z]+;base64,/, "");
+            }
+            $scope.update($scope.project).$promise.then(function (success) {
+                $location.path('/project/:id', $scope.project.id);
+            }, function (error) {
+                $scope.error = true;
+            });
+        };
 
         var allProjects = function() {
             return Project.query();
