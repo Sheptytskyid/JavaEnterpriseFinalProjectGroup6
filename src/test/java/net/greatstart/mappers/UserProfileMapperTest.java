473090ea9046465908beb70edd5cd6ea5eba3a26
AlexPoliteRudenko <rudenko.lesha@gmail.com>
2017-06-02 07:44:09 +0300

    add test and minor remarks in UserProfileMapperTest

--- src/test/java/net/greatstart/mappers/UserProfileMapperTest.java
+++ src/test/java/net/greatstart/mappers/UserProfileMapperTest.java
@@ -26,12 +26,21 @@ public class UserProfileMapperTest {
         assertEquals(contact.getPhoneNumber(), dtoUser.getPhoneNumber());
         assertArrayEquals(user.getPhoto(), dtoUser.getPhoto());
 //        assertArrayEquals(user.getOwnedProjects(), dtoUser.getOwnedProjects());
-//        assertArrayEquals(user.getInvestments(), dtoUser.getDtoInvestments());
+        assertEquals(user.getInvestments().get(0).getId(), dtoUser.getDtoInvestments().get(0).getId());
+        assertEquals(user.getInvestments().get(1).getId(), dtoUser.getDtoInvestments().get(1).getId());
+        assertEquals(user.getInvestments().get(0).getDateOfInvestment(),
+                dtoUser.getDtoInvestments().get(0).getDateOfInvestment());
+        assertEquals(user.getInvestments().get(1).getDateOfInvestment(),
+                dtoUser.getDtoInvestments().get(1).getDateOfInvestment());
+        assertEquals(user.getInvestments().get(0).getSum(), dtoUser.getDtoInvestments().get(0).getSum());
+        assertEquals(user.getInvestments().get(1).getSum(), dtoUser.getDtoInvestments().get(1).getSum());
+//        assertEquals(user.getInvestments().get(0).getProject().getId(),
+//                dtoUser.getDtoInvestments().get(0).getProject().getId());
     }
 
     @Test
     public void fromDtoProfileToUser() throws Exception {
-        DtoUserProfile dtoUser = getTestDtoUserProfile();
+        DtoUserProfile dtoUser = getFullTestDtoUserProfile();
         User user = userMapper.fromDtoProfileToUser(dtoUser);
         assertNotNull(user);
         Contact contact = user.getContact();
@@ -43,6 +52,13 @@ public class UserProfileMapperTest {
         assertEquals(dtoUser.getAddress(), contact.getAddress());
         assertEquals(dtoUser.getPhoneNumber(), contact.getPhoneNumber());
         assertArrayEquals(dtoUser.getPhoto(), user.getPhoto());
+    }
+
+    @Test
+    public void fromUserToDtoUserProfileFull() throws Exception{
+        User user = getFullTestUser();
+        DtoUserProfile dtoUser = userMapper.fromUserToDtoProfile(user, CONTEXT);
+        assertEquals(dtoUser, getFullTestDtoUserProfile());
     }
 
 }
