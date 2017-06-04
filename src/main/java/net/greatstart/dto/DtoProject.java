e18ddc52f34e7b8fbe13214be5b2373653bb1e44
holkwen <vlad.kryshtalov@gmx.com>
2017-06-04 22:52:26 +0300

    Removed equals / hashcode

--- src/main/java/net/greatstart/dto/DtoProject.java
+++ src/main/java/net/greatstart/dto/DtoProject.java
@@ -19,36 +19,4 @@ public class DtoProject {
 
     private List<DtoInvestment> dtoInvestments;
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-
-        DtoProject that = (DtoProject) obj;
-
-        if (id != null ? !id.equals(that.id) : that.id != null) {
-            return false;
-        }
-        if (category != null ? !category.equals(that.category) : that.category != null) {
-            return false;
-        }
-        if (desc != null ? !desc.equals(that.desc) : that.desc != null) {
-            return false;
-        }
-        return owner != null ? owner.equals(that.owner) : that.owner == null;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = super.hashCode();
-        result = 31 * result + (id != null ? id.hashCode() : 0);
-        result = 31 * result + (category != null ? category.hashCode() : 0);
-        result = 31 * result + (desc != null ? desc.hashCode() : 0);
-        result = 31 * result + (owner != null ? owner.hashCode() : 0);
-        return result;
-    }
 }
