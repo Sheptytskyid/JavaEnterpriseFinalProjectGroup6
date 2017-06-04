90422c7fc62a484a267e570d371e0f6610c5b0cc
holkwen <vlad.kryshtalov@gmx.com>
2017-06-04 22:19:30 +0300

    Investment test fix

--- src/test/java/net/greatstart/controllers/InvestmentControllerTest.java
+++ src/test/java/net/greatstart/controllers/InvestmentControllerTest.java
@@ -51,7 +51,7 @@ public class InvestmentControllerTest {
         dtoInvestment = getTestDtoInvestment(TEST_INVEST_1, TEST_VALUE_1, TEST_COST_1, TEST_MIN_INVEST_1);
     }
 
-    @Test(timeout = 4000)
+    @Test
     public void saveValidInvestmentShouldPassValidationAndInvokeSaveServiceMethodAndReturnHttpStatusOk()
             throws Exception {
         //init
